f80449514fa352f2af2d4f190495dec8f1658d2c
galtsev_o <galtsev_o@bsu.edu.ru>
2022-02-10 15:29:31 +0300

    vlds

---
+++ frontend/src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelloWorld = () => {
+
+    function sayHello() {
+        alert('Hello, World!');
+    }
+
+    return (
+        <button onClick={sayHello}>Click me!11133455566678910</button>
+    );
+};
+
+export default HelloWorld;
